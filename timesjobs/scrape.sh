429719efc0db54f2b69c62df12fd221694a2c036
Kunal Mangal <kmangal0991@gmail.com>
2021-06-27 12:29:31 +0530

    Update scrape.sh

--- timesjobs/scrape.sh
+++ timesjobs/scrape.sh
@@ -10,7 +10,7 @@ echo "Log file date: ${filedate}"
 cd /home/ec2-user/jobs_scraping/timesjobs/
 
 # Rum the scraper in a backgorund process. Pass the filedate as an argument
-nohup timesjobs_scrape.sh ${filedate} > log/${filedate}.log &
+nohup sh timesjobs_scrape.sh ${filedate} > log/${filedate}.log &
 
 exit 0
 
