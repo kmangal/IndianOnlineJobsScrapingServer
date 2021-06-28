d858eabf3b2fc29efa1d157e9a1201f207a54855
Kunal Mangal <kmangal0991@gmail.com>
2021-06-28 12:33:23 +0530

    Update test_imports.py
    Handles unix and windows

--- util/test_imports.py
+++ util/test_imports.py
@@ -6,6 +6,7 @@ sys.path.append('../')
 
 jobs_scraping_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
+
 def is_exception(modulepath):
     ''' Ignore certain files and folders'''
     
@@ -23,8 +24,13 @@ for root, dirs, files in os.walk(jobs_scraping_folder, topdown=False):
     for name in files:
         if name.endswith('.py') and name != "__init__.py":
         
-            folder = root.split('jobs_scraping\\')[1]
-            importpath = folder.replace('\\', '.')
+            if os.name == 'nt':
+                folder = root.split('jobs_scraping\\')[1]
+                importpath = folder.replace('\\', '.')
+            else:
+                folder = root.split('jobs_scraping/')[1]
+                importpath = folder.replace('/', '.')
+                
             scriptname = name.split('.py')[0]
             modulepath = importpath + '.' + scriptname
             
