cfc3a653a3f5c4bff054358e1c6c683e61d1aead
Kunal Mangal <kmangal0991@gmail.com>
2021-06-28 17:12:50 +0530

    Update startup.sh
    Git username and email (EC2 asks)

--- util/startup.sh
+++ util/startup.sh
@@ -8,9 +8,9 @@ git config --global credential.helper store
 git clone https://github.com/kmangal/jobs_scraping.git
 # Provide username and personal access token
 
+git config --global user.name "EC2 Server"
+git config --global user.email india.labourinsights.server@gmail.com
+
 cd jobs_scraping
 sh util/makefolders.sh
-
-pip install scrapy
-pip install bs4
 
