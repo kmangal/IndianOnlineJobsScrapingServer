a05496d0687c537cdbeea7c248654ed25ed34d47
Kunal Mangal <kmangal0991@gmail.com>
2021-06-28 12:14:03 +0530

    Update makefolders.sh

--- util/makefolders.sh
+++ util/makefolders.sh
@@ -1,14 +1,14 @@
 cd ~/jobs_scraping/
 
-for f in "teamlease" "timesjobs" "shine" "monster"
+for var in teamlease timesjobs shine monster
 do
-	mkdir ${var}/log
-	mkdir ${var}/output
-	mkdir ${var}/output/mainpage
-	mkdir ${var}/output/jobcount
-	mkdir ${var}/output/details
-	mkdir ${var}/test
-	mkdir ${var}/test/mainpage
-	mkdir ${var}/test/jobcount
-	mkdir ${var}/test/log
+	mkdir $var/log
+	mkdir $var/output
+	mkdir $var/output/mainpage
+	mkdir $var/output/jobcount
+	mkdir $var/output/details
+	mkdir $var/test
+	mkdir $var/test/mainpage
+	mkdir $var/test/jobcount
+	mkdir $var/test/log
 done
