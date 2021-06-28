6ae767f2e3120abc53280e1696c912733a73f06a
Kunal Mangal <kmangal0991@gmail.com>
2021-06-28 22:52:43 +0530

    Update worker.py

--- worker.py
+++ worker.py
@@ -6,8 +6,6 @@ import config
 import util.scrapelogger as scrapelogger
 import util.export_to_dropbox
 
-queue = Queue(connection = config.redis)
-
 logger = scrapelogger.ErrorLogger('error-log', 'log/error/errors.log')
 
 def write_error_log(job, exc_type, exc_value, traceback):
@@ -15,7 +13,9 @@ def write_error_log(job, exc_type, exc_value, traceback):
         "Uncaught exception for {func}({args})".format(func = job.func_name, args = str(job.kwargs)),
         exc_info=(exc_type, exc_value, exc_traceback))
 
+queue = Queue(connection = config.redis, exc_handler=write_error_log)
+
 # Start a worker
 with Connection(connection = config.redis):
-    worker = Worker([queue], connection = config.redis, name='worker1', exception_handlers=[write_error_log])
+    worker = Worker([queue], connection = config.redis, name='worker1')
     worker.work()
