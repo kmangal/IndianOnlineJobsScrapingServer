68f7b0060595d1435a6a2f7a51a0b49b6fe1cd7f
Kunal Mangal <kmangal0991@gmail.com>
2021-06-28 18:14:04 +0530

    Tasks test

--- start_tasks.py
+++
@@ -1,9 +0,0 @@
-from redis import Redis
-from rq import Queue
-
-import waahjobs
-
-REDIS_ENDPOINT = 'task-queue.3g7al0.0001.aps1.cache.amazonaws.com'
-
-q = Queue(connection=Redis(host=ENDPOINT, port=6379, username='default'))
-result = q.enqueue(waahjobs.waahjobs_scrape.run_scrape)
