1304d24f645b7197ecd703a58fe71ba54ceeb816
Kunal Mangal <kmangal0991@gmail.com>
2021-06-29 18:08:08 +0530

    Update main.py
    Convert from cron to schedule

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from redis import Redis
 from rq import Queue
 from rq_scheduler import Scheduler
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import tasks
 
@@ -19,50 +19,59 @@ def main():
 
     scheduler = Scheduler(connection= config.redis)
     
+    currentdate = datetime.utcnow()
+    base_start_time = datetime(year = currentdate.year, month = currentdate.month, day = currentdate.day, hour = 18, minute = 30, second = 0)
+
+    
     scheduled_jobs = [job.meta['site'] for job in scheduler.get_jobs()]
             
     if 'waahjobs' not in scheduled_jobs:
-        scheduler.cron(
-            '30 18 * * *',                                        # A cron string (e.g. "0 0 * * 0")
+        
+        scheduler.schedule(
+            scheduled_time=  base_start_time,                            # Time for first execution, in UTC timezone
             func= tasks.waahjobs_scrape,                  # Function to be queued
             kwargs={'test': False},                               # Keyword arguments passed into function when executed
+            interval = 60 * 24 * 4,                               # Interval in seconds
             repeat= None,                                         # Repeat this number of times (None means repeat forever)
             queue_name= 'default',                                # In which queue the job should be put in
             meta={'site': 'waahjobs'},                                  # Arbitrary pickleable data on the job itself
-            use_local_timezone= True                              # Interpret hours in the local timezone
         )
 
-    
+    if 'timesjobs' not in scheduled_jobs:
+        
+        scheduler.schedule(
+            scheduled_time = base_start_time,                            # Time for first execution, in UTC timezone
+            func= tasks.timesjobs_scrape,                  # Function to be queued
+            interval = 60 * 24 * 4,                               # Interval in seconds
+            repeat= None,                                         # Repeat this number of times (None means repeat forever)
+            queue_name= 'default',                                # In which queue the job should be put in
+            meta={'site': 'timesjobs'},                                  # Arbitrary pickleable data on the job itself
+        )    
+        
     if 'monster' not in scheduled_jobs:
-        scheduler.cron(
-            '30 18 1/4 * *',                                        # A cron string (e.g. "0 0 * * 0")
+        # Start with one day delay
+        
+        scheduler.schedule(
+            scheduled_time = base_start_time + timedelta(days=1),                            # Time for first execution, in UTC timezone
             func= tasks.monster_scrape,                  # Function to be queued
+            interval = 60 * 24 * 4,                               # Interval in seconds
             repeat= None,                                         # Repeat this number of times (None means repeat forever)
             queue_name= 'default',                                # In which queue the job should be put in
             meta={'site': 'monster'},                                  # Arbitrary pickleable data on the job itself
-            use_local_timezone= True                              # Interpret hours in the local timezone
         )
         
         
     if 'shine' not in scheduled_jobs:
-        scheduler.cron(
-            '30 18 2/4 * *',                                        # A cron string (e.g. "0 0 * * 0")
-            func= tasks.shine_scrape,                               # Function to be queued
+        # Start with two day delay
+        
+        scheduler.schedule(
+            scheduled_time = base_start_time +  + timedelta(days=2),        # Time for first execution, in UTC timezone
+            func= tasks.shine_scrape,                                       # Function to be queued
+            interval = 60 * 24 * 4,                               # Interval in seconds
             repeat= None,                                         # Repeat this number of times (None means repeat forever)
             queue_name= 'default',                                # In which queue the job should be put in
             meta={'site': 'shine'},                                  # Arbitrary pickleable data on the job itself
-            use_local_timezone= True                              # Interpret hours in the local timezone
         )
-
-    if 'timesjobs' not in scheduled_jobs:
-        scheduler.cron(
-            '30 18 3/4 * *',                                        # A cron string (e.g. "0 0 * * 0")
-            func= tasks.timesjobs_scrape,                               # Function to be queued
-            repeat= None,                                         # Repeat this number of times (None means repeat forever)
-            queue_name= 'default',                                # In which queue the job should be put in
-            meta={'site': 'timesjobs'},                                  # Arbitrary pickleable data on the job itself
-            use_local_timezone= True                              # Interpret hours in the local timezone
-        )        
     
 if __name__ == '__main__':
     main()
