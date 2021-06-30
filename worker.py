c4992be896bf50063fa11894c5f8086c1f4d9e29
Kunal Mangal <kmangal0991@gmail.com>
2021-06-30 08:46:05 +0530

    Update worker.py
    Spin multiple workers

--- worker.py
+++ worker.py
@@ -13,12 +13,14 @@ def write_error_log(job, exc_type, exc_value, exc_traceback):
         exc_info=(exc_type, exc_value, exc_traceback))
 
 queue = Queue(connection = config.redis)
+workercount = Worker.count(connection=config.redis)
 
 # Start a worker
 with Connection(connection = config.redis):
     worker = Worker([queue], 
                     connection = config.redis, 
-                    name='worker1', 
+                    name='worker{}'.format(workercount+1), 
                     exception_handlers=[write_error_log],
                     disable_default_exception_handler=True)
+                        
     worker.work()
