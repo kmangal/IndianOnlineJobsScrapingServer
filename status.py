9321e4acc05620404e3f685f1686aa7b2b3f3744
Kunal Mangal <kmangal0991@gmail.com>
2021-06-30 11:37:38 +0530

    Update status.py

--- status.py
+++ status.py
@@ -5,17 +5,20 @@ import humanize
 
 import config
 
+def format_time(time):
+    return time.strftime('%m/%d/%Y %I:%M:%S %p %Z')
+
 # Returns all workers registered in this connection
 workers = Worker.all(connection=config.redis)
 
 for worker in workers:
     print("Worker:", worker.name)
     print(" -state:", worker.state)
-    print(" -birth:", worker.birth_date)
+    print(" -birth:", format_time(worker.birth_date))
     
     current_job = worker.get_current_job()
     
     print(" -current job: {name} - started: {start} ({elapsed})".format(
         name= current_job.func_name, 
-        start= current_job.started_at,
+        start= format_time(current_job.started_at),
         elapsed= humanize.naturaltime(datetime.utcnow() - current_job.started_at)))
