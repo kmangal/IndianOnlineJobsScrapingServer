569d11fd34bca1caa33a67ad613d9ffff39cf00a
Kunal Mangal <kmangal0991@gmail.com>
2021-06-30 11:05:06 +0530

    Update status.py

--- status.py
+++ status.py
@@ -12,7 +12,10 @@ for worker in workers:
     print("Worker:", worker.name)
     print(" -state:", worker.state)
     print(" -birth:", worker.birth_date)
+    
+    current_job = worker.get_current_job()
+    
     print(" -current job: {name}  - started: {start} ({elapsed} elapsed)".format(
-        name=worker.current_job.func_name, 
-        start=worker.current_job.started_at,
-        elapsed= humanize.naturaltime(datetime.utcnow() - worker.current_job.started_at)))
+        name= current_job.func_name, 
+        start= current_job.started_at,
+        elapsed= humanize.naturaltime(datetime.utcnow() - current_job.started_at)))
