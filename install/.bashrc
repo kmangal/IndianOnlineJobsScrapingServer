2b5d0f8fc803270f1a8691b3bef8a7d4752488a1
Kunal Mangal <kmangal0991@gmail.com>
2021-07-06 15:00:00 +0530

    Update status.py
    Make bash command

--- install/.bashrc
+++ install/.bashrc
@@ -19,6 +19,7 @@ cd jobs_scraping
 
 # User specific aliases and functions
 alias jobs="python ~/jobs_scraping/util/jobs.py"
+alias status="python ~/jobs_scraping/util/status.py"
 
 function seelog { tail -n 5 "$(ls -1r $1/*.log | head -n 1)"; }
 export -f seelog
--- util/status.py
+++ util/status.py
@@ -3,6 +3,14 @@ from rq import Worker
 from datetime import datetime
 import humanize
 
+
+import os
+import sys
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
 import config
 
 def format_time(time):
