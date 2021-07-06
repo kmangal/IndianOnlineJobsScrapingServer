19240592218f9afdba546b015f44a53ffabf505d
Kunal Mangal <kmangal0991@gmail.com>
2021-07-06 12:49:59 +0530

    Update special_task.py

--- special_task.py
+++ special_task.py
@@ -16,12 +16,12 @@ def redo_timesjobs_details(infile, outfile, logfile):
        kwargs = {'infile' : infile,
         'outfile' : outfile,
         'log' : logfile},
-        job_timeout = 60 * 60 * 24 * 2)
+        job_timeout = 60 * 60 * 24 * 4)
 	
 if __name__ == '__main__':
     redo_timesjobs_details(
         infile= os.path.expanduser('~/jobs_scraping/timesjobs/output/mainpage/timesjobs_mainpage_20210630_032644.csv'),
         outfile = os.path.expanduser('~/jobs_scraping/timesjobs/output/details/timesjobs_details_20210630_032644.csv'),
-        logfile=os.path.expanduser('~/jobs_scraping/timesjobs/log/20210630_032644.log')
+        logfile= os.path.expanduser('~/jobs_scraping/timesjobs/log/20210630_032644.log')
         )
         
