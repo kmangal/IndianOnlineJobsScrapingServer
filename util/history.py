b9daf51ea5d404ed12f358d924b94d2527c98307
Kunal Mangal <kmangal0991@gmail.com>
2021-07-26 08:46:06 +0530

    Update history.py

--- util/history.py
+++ util/history.py
@@ -3,6 +3,7 @@ from rq.job import Job
 
 import sys
 import os
+import humanize
 
 def modify_path():
     currentdir = os.path.dirname(os.path.realpath(__file__))
@@ -19,15 +20,18 @@ def format_time(time):
     else:
         return "---"
     
+def job_start_time(job):
+    return job.started_at
     
 def list_jobs(registry):
     jobs = Job.fetch_many(registry.get_job_ids(), connection=config.redis)
+    jobs.sort(key=job_start_time)
 
     for job in jobs:
-        print("{f: <28} - started: {s}  ended: {e}".format(
+        print("{f: <28} - started: {s}  duration: {d}".format(
                 f = job.func_name,
                 s = format_time(job.started_at),
-                e = format_time(job.ended_at)))
+                d = humanize.naturaltime(job.ended_at - job.started_at)))
 
 
 if __name__ == '__main__':
